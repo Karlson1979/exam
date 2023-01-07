6d21982fbc1a6c5c4ec6ac3718a3d4ed00a1b18e
Karlson1979 <113626246+Karlson1979@users.noreply.github.com>
2023-01-07 11:03:03 +0200

    burger 7/1

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ let burger = document.querySelector('.menu-burger')
 let menu = document.querySelector('.menu')
 burger.addEventListener('click',(e)=> {
 burger.classList.toggle('active-burger')
-})
 menu.classList.toggle('active-list')
+})
